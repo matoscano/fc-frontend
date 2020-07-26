23ed0fad0dc076a467fde1125200407e4e7ba98b
Miguel Angel <matr05viru@gmail.com>
2020-07-26 11:28:54 +0200

    fix: heroku deploy routes

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(path.join(__dirname, "build")));
-
-// Handles any requests that don't match the ones above
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
-});
+app.use(express.static(path.join(__dirname, 'build')));
+-app.get('/', function (req, res) {
++app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
 
 // app.use(express.static(__dirname + "/"));
 app.listen(process.env.PORT || 8080);
